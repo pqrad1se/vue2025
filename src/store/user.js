d114dd5241ff1437ddd1aa8b577e9c9999158dc8
pqrad1se <mrazor110@mail.ru>
2025-04-23 20:32:28 +0300

    project_15

--- src/store/user.js
+++ src/store/user.js
@@ -1,98 +1,104 @@
 class User {
     constructor(id, email, password) {
-      this.id = id;
-      this.email = email;
-      this.password = password;
+        this.id = id;
+        this.email = email;
+        this.password = password;
     }
-  }
-  
-  export default {
+}
+
+export default {
     state: {
-      user: null,
-      loading: false,
-      error: null,
+        user: null,
+        loading: false,
+        error: null,
     },
     mutations: {
-      setUser(state, payload) {
-        console.log(payload);
-        state.user = payload;
-      },
-      clearError(state) {
-        state.error = null;
-      },
-      setLoading(state, payload) {
-        state.loading = payload;
-      },
-      setError(state, payload) {
-        state.error = payload;
-      },
+        setUser(state, payload) {
+            console.log(payload);
+            state.user = payload;
+        },
+        clearError(state) {
+            state.error = null;
+        },
+        setLoading(state, payload) {
+            state.loading = payload;
+        },
+        setError(state, payload) {
+            state.error = payload;
+        },
     },
     actions: {
-      async registerUser({ commit }, { email, password }) {
-        commit('clearError');
-        commit('setLoading', true);
-  
-        try {
-          // Имитация запроса на сервер
-          await new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const isRequestOk = Math.random() > 0.5; // 50% шанс успеха
-              if (isRequestOk) {
-                resolve();
-              } else {
-                reject(new Error('Ошибка регистрации'));
-              }
-            }, 3000); // Имитация задержки в 3 секунды
-          });
-  
-          // Если запрос успешен
-          commit('setUser', new User(1, email, password));
-          commit('setLoading', false);
-        } catch (error) {
-          // Если запрос неудачен
-          commit('setLoading', false);
-          commit('setError', error.message);
-          throw error;
+        async registerUser({ commit }, { email, password }) {
+            commit('clearError');
+            commit('setLoading', true);
+
+            try {
+                // Имитация запроса на сервер
+                await new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        const isRequestOk = Math.random() > 0.5; // 50% шанс успеха
+                        if (isRequestOk) {
+                            resolve();
+                        } else {
+                            reject(new Error('Ошибка регистрации'));
+                        }
+                    }, 3000); // Имитация задержки в 3 секунды
+                });
+
+                // Если запрос успешен
+                commit('setUser', new User(1, email, password));
+                commit('setLoading', false);
+            } catch (error) {
+                // Если запрос неудачен
+                commit('setLoading', false);
+                commit('setError', error.message);
+                throw error;
+            }
+        },
+
+        async loginUser({ commit }, { email, password }) {
+            commit('clearError');
+            commit('setLoading', true);
+
+            try {
+                // Имитация запроса на сервер
+                await new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        const isRequestOk = Math.random() > 0.5; // 50% шанс успеха
+                        if (isRequestOk) {
+                            resolve();
+                        } else {
+                            reject(new Error('Ошибка логина или пароля'));
+                        }
+                    }, 3000); // Имитация задержки в 3 секунды
+                });
+
+                // Если запрос успешен
+                commit('setUser', new User(1, email, password));
+                commit('setLoading', false);
+            } catch (error) {
+                // Если запрос неудачен
+                commit('setLoading', false);
+                commit('setError', error.message);
+                throw error;
+            }
+        },
+        logoutUser({ commit }) {
+            commit('setUser', null)
         }
-      },
-  
-      async loginUser({ commit }, { email, password }) {
-        commit('clearError');
-        commit('setLoading', true);
-  
-        try {
-          // Имитация запроса на сервер
-          await new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const isRequestOk = Math.random() > 0.5; // 50% шанс успеха
-              if (isRequestOk) {
-                resolve();
-              } else {
-                reject(new Error('Ошибка логина или пароля'));
-              }
-            }, 3000); // Имитация задержки в 3 секунды
-          });
-  
-          // Если запрос успешен
-          commit('setUser', new User(1, email, password));
-          commit('setLoading', false);
-        } catch (error) {
-          // Если запрос неудачен
-          commit('setLoading', false);
-          commit('setError', error.message);
-          throw error;
-        }
-      },
     },
     getters: {
-      user(state) {
-        return state.user;
-      },
-      loading(state) {
-        return state.loading;
-      },
-      error(state) {
-        return state.error;
-      },
+        user(state) {
+            return state.user;
+        },
+        loading(state) {
+            return state.loading;
+        },
+        error(state) {
+            return state.error;
+        },
+        isUserLoggedln(state) {
+            return state.user !== null
+        }
     },
-  };
+};
