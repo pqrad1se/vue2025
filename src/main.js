3b6cfdd8da59019b609c2ebe242c03604f7a8e7e
pqrad1se <mrazor110@mail.ru>
2025-04-23 21:19:28 +0300

    project_18

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import router from './router'
+import store from './store'
+import BuyAdModal from './views/Shared/BuyAdModal.vue'
 
-loadFonts();
+loadFonts()
 
-
-createApp(App).use(router).use(router).use(store).use(vuetify).mount("#app");
+createApp(App)
+    .use(router)
+    .use(store)
+    .use(vuetify)
+    .component("buy-ad-modal", BuyAdModal)
+    .mount('#app')
