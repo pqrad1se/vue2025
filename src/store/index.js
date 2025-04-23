a64250deb287cc6912ed5054ec314d3318447575
pqrad1se <mrazor110@mail.ru>
2025-04-23 19:49:53 +0300

    project_13

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore } from "vuex";
 import ads from "./ads";
 import user from './user'
+import shared from './shared'
 
 const store = createStore({
-    modules: {
-        ads, user
-    },
+  modules: {
+    ads, user, shared
+  },
 });
 
 export default store;
